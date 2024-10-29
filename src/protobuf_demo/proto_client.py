10351ff2ad200540e16216cda109343dd80e8893
Dean <44520474+kona3266@users.noreply.github.com>
2024-10-29 16:30:13 +0800

    Update proto_client.py

--- src/protobuf_demo/proto_client.py
+++ src/protobuf_demo/proto_client.py
@@ -2,6 +2,8 @@ import target_pb2
 import time
 import os
 import zmq
+from google.protobuf.internal.encoder import _VarintEncoder
+from google.protobuf.internal.decoder import _DecodeVarint
 
 rect = target_pb2.Rect()
 rect.x1 = 0
@@ -9,6 +11,18 @@ rect.y1 = 2
 rect.x2 = 2
 rect.y2 = 0
 url = "tcp://localhost:5555"
+
+def encode_varint(value):
+    """ Encode an int as a protobuf varint """
+    data = []
+    _VarintEncoder()(data.append, value, False)
+    return b''.join(data)
+
+
+def decode_varint(data):
+    """ Decode a protobuf varint to an int """
+    return _DecodeVarint(data, 0)[0]
+
 class Client():
     def __init__(self, ctx, data):
         context = ctx
@@ -17,10 +31,20 @@ class Client():
 
     def send(self, url):
         self.socket.connect(url)
-        self.socket.send(self.data)
+        size = encode_varint(len(self.data))
+        self.socket.send(size + self.data)
 
     def receive(self):
-        resp = self.socket.recv()
+        data = b''
+        while True:
+            try:
+                data += sef.socket.recv(1)
+                size = decode_varint(data)
+                break
+            except IndexError:
+                pass
+        # Receive the message data
+        resp = self.socket.recv(size)
         print(resp)
 
 def send_data(data):
