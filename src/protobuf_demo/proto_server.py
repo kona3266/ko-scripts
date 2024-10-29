f6f8b502067217da1a77b82dcc67c1fdb890c4e6
Dean <44520474+kona3266@users.noreply.github.com>
2024-10-29 16:36:02 +0800

    Update proto_server.py

--- src/protobuf_demo/proto_server.py
+++ src/protobuf_demo/proto_server.py
@@ -4,10 +4,27 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://localhost:5555")
 
-while True:
-    message = socket.recv()
+def handle_data(socket, message):
     recv_rec = target_pb2.Rect()
     recv_rec.ParseFromString(message)
     print(recv_rec.x1, recv_rec.y1)
     print(recv_rec.x2, recv_rec.y2)
     socket.send(b"new message")
+
+def receive_data(socket):
+    data = b''
+    while True:
+        try:
+            data += socket.recv(1)
+            size = decode_varint(data)
+            break
+        except IndexError:
+            pass
+    # Receive the message data
+    message = socket.recv(size)
+    return message
+
+while True:
+    message = socket.recv()
+    t = threading.Thread(target=send_data, args=(socket, message))
+    t.start()
